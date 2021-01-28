13bed76a8df754d58954256513890284f49c3176
yongsannnn <yong_s@live.com>
2021-01-28 10:01:27 +0000

    complete question 4

---
+++ hands-on/q4-simple-json-example/script.js
@@ -0,0 +1,11 @@
+let btn = document.querySelector("#load-image-btn");
+let wordElement = document.querySelector("#text")
+let imgElement = document.querySelector("#artwork")
+
+btn.addEventListener("click", async function(){
+    let response = await axios.get("artwork.json")
+    wordElement.innerHTML = response.data.title;
+    imgElement.innerHTML = `<img src="${response.data.image_url}"></img>`
+    console.log(response)
+
+})
