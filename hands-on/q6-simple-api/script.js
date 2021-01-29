03824f8aeef54927e0c0a4b543e66980936f8b18
yongsannnn <yong_s@live.com>
2021-01-29 06:28:17 +0000

    start of q6

--- hands-on/q6-simple-api/script.js
+++ hands-on/q6-simple-api/script.js
@@ -1,2 +1,8 @@
-
 let url = "https://programming-quotes-api.herokuapp.com/quotes/random"
+let btn = document.querySelector("#load")
+
+btn.addEventListener("click", function () {
+
+    alert("hi")
+
+}) 
