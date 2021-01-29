a0b8ce8dc231b782e4ee84a7cfc10a980768c908
yongsannnn <yong_s@live.com>
2021-01-29 02:08:11 +0000

    completed Q5 using promises

--- hands-on/q5-array-of-json-objects/script.js
+++ hands-on/q5-array-of-json-objects/script.js
@@ -21,8 +21,17 @@ btn2.addEventListener("click", function(){
     axios.get("users.json").then(function(response){
         let data = response.data
         let data1 = data.users
-        let search = document.querySelector(".input").value
-        
-
+        let search = document.querySelector(".input")
+        console.log(search)
+        for ( let d of data1){
+            console.log(d)
+            if (search.value == d.userId){
+                console.log(search.value)
+                document.querySelector("#result").innerHTML = `
+                <p>First Name: "${d.firstName}"</p>
+                <p>Last Name: "${d.lastName}"</p>
+                `
+            }
+        }
     })
 })
